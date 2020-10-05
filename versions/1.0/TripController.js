6d6e56b286a8b41788eb0f97d8aadf8892c8a814
ByronKweh <byron.kweh@mobilitech.co>
2020-10-06 03:34:22 +0800

    testing rebateAmount issue

--- versions/1.0/TripController.js
+++ versions/1.0/TripController.js
@@ -469,9 +469,9 @@ exports.qrDocklessDropCheck = function(userId,userTripId,qrString,userLat,userLn
           "rebateAmount": rebateAmount
         });
           var newTripObj = new tripModel();
-          console.log("User OBJ IS: " + _userTripObj.val())
+          console.log("User OBJ IS: " + _userTripObj.tripId);
 
-        Object.assign(newTripObj,_userTripObj.val(),{"tripId":userTripId}, {"userId": userId}, {"count": count})
+        Object.assign(newTripObj,_userTripObj.tripId,{"tripId":userTripId}, {"userId": userId}, {"count": count})
         userTransactionToReturn.push(newTripObj);
         newTripObj.feedback = mysql.escape(newTripObj.feedback);
 
